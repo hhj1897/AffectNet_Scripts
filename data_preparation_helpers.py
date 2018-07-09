0a0e151575eeef7c7d442028b3c5a24a88aea654
hhj1897 <js1907@imperial.ac.uk>
2018-07-09 20:47:58 +0100

    Finish data preparation (on thge manually annotated data).

--- data_preparation_helpers.py
+++ data_preparation_helpers.py
@@ -2,6 +2,7 @@ import os
 import cv2
 import sys
 import time
+import numpy as np
 
 ibug_face_tracker_path = 'D:\\hhj\\dlib_and_chehra_stuff'   # You'll need to change this
 sys.path.append(ibug_face_tracker_path)
@@ -45,6 +46,7 @@ def landmark_organiser(result_queue, result_list):
     while True:
         result = result_queue.get()
         if result is None:
+            result_queue.put(None)
             break
         else:
             idx = result['index']
@@ -54,4 +56,85 @@ def landmark_organiser(result_queue, result_list):
             current_time = time.time()
             if current_time - last_check_time > 30.0:
                 print('%d of %d images processed.' % (processed_images, number_of_jobs))
+                last_check_time = current_time
+
+
+def extract_face_image(image, landmarks, target_size=(60, 60), margin=(0.1, 0.1, 0.1, 0.1)):
+    # Ignore the chin points
+    if landmarks.shape[0] == 68:
+        landmarks = landmarks[17:]
+
+    # Find the face box
+    top_left = np.min(landmarks, axis=0)
+    bottom_right = np.max(landmarks, axis=0)
+    face_size = bottom_right - top_left
+    face_box = [int(np.floor(top_left[0] - face_size[0] * margin[0])),
+                int(np.floor(top_left[1] - face_size[1] * margin[1])),
+                int(np.ceil(bottom_right[0] + face_size[0] * margin[2])) + 1,
+                int(np.ceil(bottom_right[1] + face_size[1] * margin[3])) + 1]
+
+    # Make the face box square
+    difference = (face_box[3] - face_box[1]) - (face_box[2] - face_box[0])
+    if difference > 0:
+        face_box[0] -= difference // 2
+        face_box[2] += difference - difference // 2
+        pass
+    elif difference < 0:
+        difference = -difference
+        face_box[1] -= difference // 2
+        face_box[3] += difference - difference // 2
+
+    # Pad the image when necessary
+    padding = np.zeros((image.ndim, 2), dtype=int)
+    if face_box[0] < 0:
+        padding[1][0] = -face_box[0]
+    if face_box[2] > image.shape[1]:
+        padding[1][1] = face_box[2] - image.shape[1]
+    if face_box[1] < 0:
+        padding[0][0] = -face_box[1]
+    if face_box[3] > image.shape[0]:
+        padding[0][1] = face_box[3] - image.shape[0]
+    image = np.pad(image, padding, 'symmetric')
+    if face_box[0] < 0:
+        face_box[2] -= face_box[0]
+        face_box[0] = 0
+    if face_box[1] < 0:
+        face_box[3] -= face_box[1]
+        face_box[1] = 0
+
+    # Extract the face image
+    face_image = cv2.resize(image[face_box[1]: face_box[3], face_box[0]: face_box[2]],
+                            target_size, interpolation=cv2.INTER_CUBIC)
+    return face_image
+
+
+def face_extractor(job_queue, output_queue, target_size=(60, 60), margin=(0.1, 0.1, 0.1, 0.1)):
+    while True:
+        job = job_queue.get()
+        if job is None:
+            job_queue.put(None)
+            break
+        else:
+            try:
+                face_image = extract_face_image(cv2.imread(job['image_path']),
+                                                job['landmarks'], target_size, margin)
+                cv2.imwrite(job['output_path'], face_image)
+                output_queue.put(job['output_path'])
+            except:
+                pass
+
+
+def face_extractor_monitor(output_queue):
+    extract_face_images = 0
+    last_check_time = time.time()
+    while True:
+        output_path = output_queue.get()
+        if output_path is None:
+            output_queue.put(None)
+            break
+        else:
+            extract_face_images = extract_face_images + 1
+            current_time = time.time()
+            if current_time - last_check_time > 30.0:
+                print('%d face images extracted.' % extract_face_images)
                 last_check_time = current_time
